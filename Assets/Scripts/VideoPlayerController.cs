f1d4f764ac00afe01b14ade768a2a4cf52a44999
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-08-22 00:07:07 +1000

    Refactor: Updated code

--- Assets/Scripts/VideoPlayerController.cs
+++ Assets/Scripts/VideoPlayerController.cs
@@ -1,16 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Video;
 
 public class VideoPlayerController : MonoBehaviour {
+    VideoPlayer videoPlayer;
+    public GameObject hints;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        videoPlayer = GetComponent<VideoPlayer>();
+
+        // Each time we reach the end, we slow down the playback by a factor of 10.
+        videoPlayer.loopPointReached += onVideoEndReached;
+        hints.SetActive(false);
+    }
+
+    private void onVideoEndReached(VideoPlayer source)
+    {
+        hints.SetActive(false);
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void PlayVideo()
+    {
+        if (videoPlayer == null)
+        {
+            if (!videoPlayer.isPlaying) videoPlayer.Play();
+            hints.SetActive(true);
+        }
+    }
+
+    public void ResetVideo()
+    {
+        if (videoPlayer != null)
+        {
+            if (videoPlayer.isPlaying) videoPlayer.Stop();
+            hints.SetActive(false);
+        }
+    }
 }
