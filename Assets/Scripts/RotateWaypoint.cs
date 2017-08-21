27cbcfed4599841b4dbd99a7558f476dfa964919
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-08-22 00:14:04 +1000

    Feature: Added rotation animation script

---
+++ Assets/Scripts/RotateWaypoint.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateWaypoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        gameObject.transform.Rotate(0, 90*Time.fixedDeltaTime, 0);
+    }
+}
