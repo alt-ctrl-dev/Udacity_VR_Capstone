915fc045e7bde11b1dfe79d15d00bf0372e542fc
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-08-22 10:03:21 +1000

    Feature: Added audio listener code

--- Assets/Scripts/VideoPlayerController.cs
+++ Assets/Scripts/VideoPlayerController.cs
@@ -6,21 +6,33 @@ using UnityEngine.Video;
 
 public class VideoPlayerController : MonoBehaviour {
     VideoPlayer videoPlayer;
+    GvrAudioSource listener;
     public GameObject hints;
 
     // Use this for initialization
     void Start () {
         videoPlayer = GetComponent<VideoPlayer>();
 
+        listener = GetComponent<GvrAudioSource>();
+
         // Each time we reach the end, we slow down the playback by a factor of 10.
-        videoPlayer.loopPointReached += onVideoEndReached;
+        videoPlayer.loopPointReached += VideoPlayer_loopPointReached;
+        videoPlayer.started += VideoPlayer_started;
         hints.SetActive(false);
     }
 
-    private void onVideoEndReached(VideoPlayer source)
+    private void VideoPlayer_started(VideoPlayer source)
+    {
+        if (!listener.isPlaying)
+            listener.Play();
+    }
+
+    private void VideoPlayer_loopPointReached(VideoPlayer source)
     {
         hints.SetActive(false);
         gameObject.SetActive(false);
+        if (listener.isPlaying)
+            listener.Stop();
     }
 
     // Update is called once per frame
@@ -32,7 +44,10 @@ public class VideoPlayerController : MonoBehaviour {
     {
         if (videoPlayer != null)
         {
-            if (!videoPlayer.isPlaying) videoPlayer.Play();
+            if (!videoPlayer.isPlaying)
+            {
+                videoPlayer.Play();
+            }
             hints.SetActive(true);
         }
     }
@@ -41,7 +56,10 @@ public class VideoPlayerController : MonoBehaviour {
     {
         if (videoPlayer != null)
         {
-            if (videoPlayer.isPlaying) videoPlayer.Stop();
+            if (videoPlayer.isPlaying)
+            {
+                videoPlayer.Stop();
+            }
             hints.SetActive(false);
         }
     }
