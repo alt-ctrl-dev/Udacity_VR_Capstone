f8cf311e83f8dc44b1768bec2e3d27d57562c41a
Coutinho, Reuben <reuben.coutinho@accenture.com>
2017-08-24 01:04:23 +1000

    Chore: Added Panel to show photgemmetry model

---
+++ Assets/Scripts/RotateTextPanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTextPanel : MonoBehaviour {
+
+    public GameObject Player;
+    private float speed = 5.0f;
+    // Use this for initialization
+    void Start () {
+        if (Player == null) throw new System.Exception("Need a Camera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 dir = Player.transform.position - transform.position;
+        dir.y = 0; // keep the direction strictly horizontal
+        Quaternion rot = Quaternion.LookRotation(-dir);
+        // slerp to the desired rotation over time
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, speed * Time.deltaTime);
+    }
+}
